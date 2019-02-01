bf3f87190ebeef52a75929f655be6194c159d304
Andrew Philpott <andrewlphilpott@gmail.com>
2019-02-01 15:30:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ wss.on('connection', (ws) => {
   
   ws.on('message', (data) => {
     wss.clients.forEach((client) => {
-      client.send(new Date().toTimeString());
+      client.send(data);
     });
   });
 });
