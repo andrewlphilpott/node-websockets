6f85fe71804d8c5bb48f7c2dad8014796431c3f8
Andrew Philpott <andrewlphilpott@gmail.com>
2019-02-01 15:29:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
-});
-
-wss.on('message', (data) => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+  
+  ws.on('message', (data) => {
+    ws.clients.forEach((client) => {
+      client.send(new Date().toTimeString());
+    });
   });
 });
 
